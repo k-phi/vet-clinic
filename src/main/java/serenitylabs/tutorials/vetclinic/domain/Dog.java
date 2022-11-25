495fb844f4e0b415fbde8f85f5faa56a510f6702
k-phi <kraft.philipp@gmail.com>
2022-11-25 21:11:54 +0100

    Do Builders in action

--- src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
@@ -3,32 +3,58 @@ package serenitylabs.tutorials.vetclinic.domain;
 import java.time.LocalDateTime;
 
 public class Dog {
-    private String name;
-    private String breed;
-    private LocalDateTime dateOfBirth;
+    private final String name;
+    private final String breed;
+    private final LocalDateTime dateOfBirth;
+    private final String favouriteFood;
+
+    public Dog(String name, String breed, LocalDateTime dateOfBirth) {
+        this(name, breed, dateOfBirth, null);
+    }
+
+    public Dog(String name, String breed, LocalDateTime dateOfBirth, String favouriteFood) {
+        this.name = name;
+        this.breed = breed;
+        this.dateOfBirth = dateOfBirth;
+        this.favouriteFood = favouriteFood;
+    }
+
+    public static DogBuilder called(String name) {
+        return new DogBuilder(name);
+    }
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-
-        this.name = name;
     }
 
     public String getBreed() {
         return breed;
     }
 
-    public void setBreed(String breed) {
-        this.breed = breed;
-    }
-
     public LocalDateTime getDateOfBirth() {
         return dateOfBirth;
     }
 
-    public void setDateOfBirth(LocalDateTime dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
+    public String getFavouriteFood() {
+        return favouriteFood;
+    }
+
+    public static class DogBuilder {
+        private final String name;
+        private String breed;
+
+        public DogBuilder(String name) {
+            this.name = name;
+        }
+
+        public DogBuilder ofBreed(String breed) {
+            this.breed = breed;
+            return this;
+        }
+
+        public Dog bornOn(LocalDateTime dataOfBirth) {
+            return new Dog(name, breed, dataOfBirth);
+        }
+
     }
 }
