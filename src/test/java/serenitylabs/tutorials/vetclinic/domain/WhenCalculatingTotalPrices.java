f605044f10923048b501b54ece1097232cfa47f9
k-phi <kraft.philipp@gmail.com>
2022-11-26 16:49:06 +0100

    Do tutorial

--- src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenCalculatingTotalPrices.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenCalculatingTotalPrices.java
@@ -2,12 +2,13 @@ package serenitylabs.tutorials.vetclinic.domain;
 
 import org.junit.Test;
 
-import org.junit.Assert;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 public class WhenCalculatingTotalPrices {
 
     @Test
-    public void total_consultation_price_should_include_20_percent_tax() throws Exception {
+    public void total_consultation_price_should_include_20_percent_tax() {
         // GIVEN
         int netPrice = 100;
 
@@ -15,6 +16,6 @@ public class WhenCalculatingTotalPrices {
         int totalPrice = TotalConsultationPrice.includingTax().forANetPriceOf(netPrice);
 
         // THEN
-        Assert.assertEquals(120, totalPrice);
+        assertThat(totalPrice, equalTo(120));
     }
 }
--- src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
@@ -3,14 +3,37 @@ package serenitylabs.tutorials.vetclinic.domain;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
 public class WhenWeCreateANewDog {
 
     @Test
-    public void a_new_dog_should_have_a_name_and_a_breed_and_a_colour() throws Exception {
+    public void a_new_dog_should_have_a_name_and_a_breed_and_a_colour() {
         Dog fido = Dog.called("Fido").ofBreed("Labrador").andOfColour("Black");
 
-        Assert.assertEquals("Fido",fido.getName());
+        Assert.assertEquals("Fido", fido.getName());
         Assert.assertEquals("Labrador", fido.getBreed());
-        Assert.assertEquals("Black", fido.getColour());
+        assertThat(fido.getColour(), contains("Black"));
+    }
+
+    @Test
+    public void a_dog_should_be_printed_in_a_readable_form() {
+        Dog fido = Dog.called("Fido").ofBreed("Labrador").andOfColour("Black");
+
+        assertThat(fido.toString(), is(equalToIgnoringCase("Fido the Black Labrador")));
+        assertThat(fido.toString(), startsWith("Fido"));
+        assertThat(fido.toString(), endsWith("labrador"));
+        assertThat(fido.toString(), containsString("black"));
+    }
+
+    @Test
+    public void a_dog_can_have_several_colours() {
+        Dog fido = Dog.called("Fido").ofBreed("Labrador").andOfColour("Black", "White");
+
+        assertThat(fido.getColour(), contains("Black", "White"));
+        assertThat(fido.getColour(), hasItem("Black"));
+        assertThat(fido.getColour(), not(hasItem("Red")));
+        assertThat(fido.toString(), is(equalToIgnoringCase("Fido the Black and White Labrador")));
     }
 }
